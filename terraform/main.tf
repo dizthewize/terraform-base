8b3d2f9ce80650f6c258ddb9013deff65e5c72b3
Martez Howard <conner.martez@gmail.com>
2022-06-12 21:52:20 -0700

    @feat/ec2: add ec2 deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,98 @@ resource "aws_subnet" "myapp-subnet-1" {
     Name: "${var.env_prefix}-subnet-1"
   }
 }
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-main-rtb"
+  }
+}
+
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    cidr_blocks = [var.my_ip]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-default-sg"
+  }
+}
+
+data "aws_ami" "lastest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-*-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.lastest-amazon-linux-image.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  /* Use `$pwd` command to get full path && add ssh id_rsa public key */
+  public_key = file(var.my_public_key)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.lastest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+  ### Script file run after instance is setup ###
+  user_data = file("entry-script.sh")
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,6 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable my_public_key {}
